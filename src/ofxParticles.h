170351781375b41de0725f7047f2c17a62e9b34d
Timothy Scaffidi <timothy.scaffidi@gmail.com>
2017-03-10 22:51:15 -0500

    added fast sqrt and binning

--- src/ofxParticles.h
+++ src/ofxParticles.h
@@ -16,6 +16,15 @@ inline ofVec3f ofRandVec3f() {
     return ofVec3f(ofRandomf(),ofRandomf(),ofRandomf()).normalize().scale(ofRandomf());
 }
 
+inline float fastSqrt(float x) {
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;            // store floating-point bits in integer
+    i = 0x5f3759df - (i >> 1);    // initial guess for Newton's method
+    x = *(float*)&i;              // convert new bits into float
+    x = x*(1.5f - xhalf*x*x);     // One round of Newton's method
+    return 1.0f / x;
+}
+
 class ofxParticle {
 public:
     ofVec3f position;
@@ -41,9 +50,9 @@ public:
     ofxParticle() {
         position = velocity = acceleration = ofVec3f(0,0,0);
         color = ofColor(255,255,255,255);
-        mass = 1.0;
-        size = 1.0;
-        lifeStart = life = 1.0;
+        mass = 1.0f;
+        size = 1.0f;
+        lifeStart = life = 1.0f;
         particleID = 0;
         dt = 1.0/60;
     }
@@ -53,11 +62,11 @@ public:
         velocity = vel;
         acceleration = ofVec3f(0,0,0);
         color = ofColor(255,255,255,255);
-        mass = 1.0;
+        mass = 1.0f;
         size = size_;
         lifeStart = life = life_;
         particleID = 0;
-        dt = 1.0/60;
+        dt = 1.0f / 60.0f;
     }
 
     ofxParticle(const ofxParticle &mom) {
@@ -72,7 +81,7 @@ public:
         life = mom.life;
         lifeStart = mom.lifeStart;
         particleID = mom.particleID;
-        dt = 1.0/60;
+        dt = 1.0f / 60.0f;
     }
 
     ~ofxParticle() {}
@@ -90,7 +99,7 @@ public:
         life = mom.life;
         lifeStart = mom.lifeStart;
         particleID = mom.particleID;
-        dt = 1.0/60;
+        dt = 1.0f / 60.0f;
         return *this;
     }
 
@@ -111,7 +120,7 @@ public:
             position.x = minPoint.x+1;
         }
         if (position.x < minPoint.x + dist) {
-            dir = ofVec3f(1,0,0);
+            dir = ofVec3f(1.0f, 0.0f, 0.0f);
             dis = position.x - minPoint.x;
             dis = dist - dis;
             force = -k * dis * spring_constant;
@@ -123,7 +132,7 @@ public:
                 position.x = maxPoint.x-1;
             }
             if (position.x > maxPoint.x - dist) {
-                dir = ofVec3f(-1,0,0);
+                dir = ofVec3f(-1.0f, 0.0f, 0.0f);
                 dis = maxPoint.x - position.x;
                 dis = dist - dis;
                 force =  -k * dis * spring_constant;
@@ -137,7 +146,7 @@ public:
             position.y = minPoint.y+1;
         }
         if (position.y < minPoint.y + dist) {
-            dir = ofVec3f(0,1,0);
+            dir = ofVec3f(0.0f, 1.0f, 0.0f);
             dis = position.y - minPoint.y;
             dis = dist - dis;
             force = -k * dis * spring_constant;
@@ -149,7 +158,7 @@ public:
                 position.y = maxPoint.y-1;
             }
             if (position.y > maxPoint.y - dist) {
-                dir = ofVec3f(0,-1,0);
+                dir = ofVec3f(0.0f, -1.0f, 0.0f);
                 dis = maxPoint.y - position.y;
                 dis = dist - dis;
                 force =  -k * dis * spring_constant;
@@ -164,16 +173,23 @@ public:
 
     // void spring(ofPoint p, const float k, const float springDist, const float drag) { spring(p, k, springDist, drag); }
     float springBoth(ofxParticle * p, const float k, const float springDist, const float drag = 1.0f, const float springSnap = 1.0f) {
-        float dist = position.distance(p->position);
-        if(dist > springDist * springSnap) return 0.0;
+        if (abs(position.x - p->position.x) > springDist * springSnap || abs(position.y - p->position.y) > springDist * springSnap) return 0.0f;
 
+        float dist = position.squareDistance(p->position);
+        if(dist > springDist * springDist * springSnap) return 0.0f;
+
+        dist = fastSqrt(dist);
         dist = springDist - dist;
+
         float force = (-k / (mass * p->mass)) * (dist / drag);
 
-        ofVec3f dir = ((ofVec3f)(p->position - position)).getNormalized();
+        ofVec3f dir = ((ofVec3f)(p->position - position));
+        dir = (dir / fastSqrt(dir.lengthSquared()));
 
-        acceleration += dir * (force / mass);
-        p->acceleration -= dir * (force / p->mass);
+        acceleration += dir * force * dt;
+        p->acceleration -= dir * force * dt;
+        // acceleration += dir * (force * dt / mass);
+        // p->acceleration -= dir * (force * dt / p->mass);
         return -force;
     }
 
@@ -201,7 +217,7 @@ public:
             if(consumeParticle)
                 life = 0;
         }
-        dir.normalize().scale( gravity * mass * mass2 * dt * (1.0/(dist)));
+        dir.normalize().scale( gravity * mass * mass2 * dt * (1.0f/(dist)));
         acceleration += dir;
     }
 
@@ -236,10 +252,11 @@ public:
 
     void update(const float timeStep, const float drag) {
         dt = timeStep;
-        velocity += acceleration * dt;
-        velocity -= (velocity * dt * (1.0-drag));
+        ofVec3f a = (acceleration * dt) / mass;
+        velocity += a;
+        velocity -= (velocity * dt * (1.0f - drag));
         position += velocity * dt;
-        acceleration -= acceleration * dt;
+        acceleration -= a;
         rotation += rotationalVelocity * dt;
 
         life -= dt;
@@ -434,8 +451,84 @@ public:
 
     int getNumParticles() {return numParticles;}
 
+    void binParticles(vector<ofxParticle *> particles, const int binPower, const ofRectangle bounds) {
+        if (this->binPower != binPower || bounds != binBounds) {
+            this->binPower = binPower;
+
+            float binSize = (1 << binPower);
+
+            xbins = ceilf(bounds.width / binSize);
+            ybins = ceilf(bounds.height / binSize);
+
+            bins.resize(xbins * ybins);
+        }
+
+        for (int i = 0; i < bins.size(); ++i) {
+            bins[i].clear();
+        }
+
+        int xbin, ybin;
+        ofxParticle * p;
+        for (int i = 0; i < particles.size(); ++i) {
+            p = particles[i];
+            if (bounds.inside(p->position)) {
+                xbin = ((unsigned) p->position.x) >> binPower;
+                ybin = ((unsigned) p->position.y) >> binPower;
+                bins[xbin + ybin * xbins].push_back(p);
+            }
+        }
+    }
+
+    int getNeighbors(vector<ofxParticle *> &neighbors, ofxParticle &p, const float radius) {
+        return getNeighbors(neighbors, p.position.x, p.position.y, radius);
+    }
+
+    int getNeighbors(vector<ofxParticle *> &neighbors, float targetX, float targetY, const float radius) {
+        //adapted from kylemcdonalds binned particle system
+        int x, y, p, bindex, minX, minY, maxX, maxY;
+        unsigned int minXBin, maxXBin, minYBin, maxYBin;
+        minX = (int)(targetX - radius);
+        minX = minX < 0 ? 0 : minX;
+        maxX = (int)(targetX + radius);
+        minY = (int)(targetY - radius);
+        minY = minY < 0 ? 0 : minY;
+        maxY = (int)(targetY + radius);
+        minXBin = minX >> binPower;
+        maxXBin = maxX >> binPower;
+        minYBin = minY >> binPower;
+        maxYBin = maxY >> binPower;
+        maxXBin++;
+        maxYBin++;
+
+        if(maxXBin > xbins) {
+            maxXBin = xbins;
+        }
+        if(maxYBin > ybins) {
+            maxYBin = ybins;
+        }
+
+        neighbors.clear();
+
+        for (y = minYBin; y < maxYBin; y++) {
+            for (x = minXBin; x < maxXBin; x++) {
+                bindex = x + y * xbins;
+                for (p = 0; p < bins[bindex].size(); p++) {
+                    neighbors.push_back(bins[bindex][p]);
+                }
+
+            }
+        }
+        return neighbors.size();
+    }
+
+
 private:
     list<ofxParticle*> particles;
     int numParticles;
     int totalParticlesEmitted;
+
+    vector<vector<ofxParticle *>> bins;
+    ofRectangle binBounds;
+    int binPower;
+    int xbins, ybins;
 };
